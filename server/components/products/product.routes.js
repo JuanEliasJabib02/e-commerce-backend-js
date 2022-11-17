2c6b2ad8f35a9bf893c225b22c9a35d4b2c044d4
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-11-17 15:47:31 -0500

    addProductValidator

--- server/components/products/product.routes.js
+++ server/components/products/product.routes.js
@@ -1,30 +1,29 @@
-
-const express = require("express");
-const { checkToken } = require("../../middlewares/checkToken");
-const { onlyAdmin } = require("../../middlewares/onlyAdmin");
-const { addProduct } = require("./products.controller");
+const express = require('express');
 
 //Controllers
+const { addProduct } = require('./products.controller');
 
 //Middlewares
+const { addProductValidator } = require('./middlewares/products.validators');
+const { checkToken } = require('../../middlewares/checkToken');
+const { onlyAdmin } = require('../../middlewares/onlyAdmin');
 
-
+//Router
 const productsRouter = express.Router();
 
-
 // Endpoints
-
-
-//add product (ONLY ADMIN)
-productsRouter.post("/add-product", checkToken, onlyAdmin, addProduct);
+productsRouter.post(
+	'/add-product',
+	checkToken,
+	onlyAdmin,
+	addProductValidator,
+	addProduct
+);
 
 //add productColorSize(ONLY ADMIN)
 
 //add productColor(ONLY ADMIN)
 
 //add productSize (Only ADMIN)
- 
-
-
 
 module.exports = { productsRouter };
--- server/components/products/products.controller.js
+++ server/components/products/products.controller.js
@@ -1,18 +1,10 @@
-
-
 const addProduct = async (req, res, next) => {
-  
-  try {
-    
-    const { name, details, categoryId, price } = req.body;
-
-    
-
-  } catch (err) {
-    next(err);
-  }
-}
-
-
+	try {
+		const { name, details, categoryId, price } = req.body;
+		console.log(name, details, categoryId, price);
+	} catch (err) {
+		next(err);
+	}
+};
 
 module.exports = { addProduct };
