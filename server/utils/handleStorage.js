945a4c0d3b5a41dceeb13cd255273c7f7023245c
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2022-12-03 16:40:48 -0500

    handle storage working2

--- server/utils/handleStorage.js
+++ server/utils/handleStorage.js
@@ -1,15 +1,13 @@
 const multer = require('multer');
-
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		const pathStorage = __dirname;
-		console.log(pathStorage);
+		const pathStorage = `${__dirname}/../storage`;
 		cb(null, pathStorage);
 	},
 	filename: function (req, file, cb) {
-		const ext = file.originalNname.split('.').pop();
-		const fileName = `file- ${Date.now()}.${ext}}`;
-		cb(null, fileName);
+		const ext = file.originalname.split('.').pop();
+		const filename = `file-${Date.now()}.${ext}`;
+		cb(null, filename);
 	},
 });
 
